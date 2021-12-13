cb36a3114f8ea4cbf7cc253e07d658d4f2df5fa6
Kat <kyevsukov@gmail.com>
2021-12-13 17:07:51 -0500

    wrote all model functions

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -10,17 +10,24 @@ function find() {
 /**
   resolves to an ARRAY with all users that match the filter condition
  */
-function findBy(filter) {}
+function findBy(filter) {
+  return db("users").where(filter)
+}
 
 /**
   resolves to the user { user_id, username } with the given user_id
  */
-function findById(user_id) {}
+function findById(user_id) {
+  return db("users").where({ user_id }).first()
+}
 
 /**
   resolves to the newly inserted user { user_id, username }
  */
-function add(user) {}
+async function add(user) {
+  const [id] =  await db ("users").insert(user)
+  return findById(id)
+}
 
 // Don't forget to add these to the `exports` object so they can be required in other modules
-module.exports = { find }
+module.exports = { find, findBy, findById, add }
